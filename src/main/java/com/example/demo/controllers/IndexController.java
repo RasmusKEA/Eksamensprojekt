f033a4c5acd60b8b1a7fe5601e14e558ff9f5e61
Rasmus Møller <rasm937k@stud.kea.dk>
2020-11-30 11:17:12 +0100

    flash messages, login and create

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -5,6 +5,7 @@ import com.example.demo.services.UserServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -27,17 +28,37 @@ public class IndexController {
     }
 
     @PostMapping("registerPost")
-
-    public String registerPost(HttpServletRequest request){
+    public String registerPost(HttpServletRequest request, RedirectAttributes redirectAttributes){
         UserServices us = new UserServices();
         String password1 = request.getParameter("password1");
         String password2 = request.getParameter("password2");
 
-        if(us.registerPasswordMatch(password1, password2)){
-            ur.createUser(request.getParameter("fullname"), request.getParameter("email"), password1);
-            return "login";
-        }
 
-        return "register";
+
+        if(us.registerPasswordMatch(password1, password2)){
+            boolean succCreate = ur.createUser(request.getParameter("fullname"), request.getParameter("email"), password1);
+            if(succCreate){
+                redirectAttributes.addFlashAttribute("createMessage", "Din konto er nu blevet oprettet og kan nu logge ind");
+                return "redirect:/login";
+            }
+        }
+        redirectAttributes.addFlashAttribute("failCreateMsg", "Email er allerede i systemet");
+        return "redirect:/register";
+    }
+
+    @PostMapping("loginPost")
+    public String loginPost(HttpServletRequest request, RedirectAttributes redirectAttributes){
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        boolean correctLogin = ur.loginUser(email, password);
+
+        if(correctLogin){
+            return "redirect:/dashboard";
+        }
+        redirectAttributes.addFlashAttribute("loginMessage", "Forkert email eller kodeord. Prøv igen");
+        return "redirect:/login";
+
     }
 }
--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -3,26 +3,55 @@ package com.example.demo.repositories;
 import com.example.demo.services.DBConnection;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
 
 public class UserRepository {
 
-    public void createUser(String fullname, String email, String password){
+    public boolean createUser(String fullname, String email, String password){
         DBConnection connection = new DBConnection();
 
         try {
-          PreparedStatement ps = connection.establishConnection().prepareStatement("INSERT INTO users (fullname, email, password) VALUES (?, ?, ?)");
-          ps.setString(1,fullname);
-          ps.setString(2, email);
-          ps.setString(3, password);
-          ps.executeUpdate();
+            PreparedStatement ps1 = connection.establishConnection().prepareStatement("SELECT email FROM users");
+            ResultSet rs = ps1.executeQuery();
+
+            while (rs.next()){
+                if(email.equals(rs.getString(1))){
+                    return false;
+                }
+            }
+            PreparedStatement ps = connection.establishConnection().prepareStatement("INSERT INTO users (fullname, email, password) VALUES (?, ?, ?)");
+            ps.setString(1,fullname);
+            ps.setString(2, email);
+            ps.setString(3, password);
+            ps.executeUpdate();
         } catch (SQLException e) {
             if(e instanceof SQLIntegrityConstraintViolationException){
                 System.out.println("Email is already in use " + e.getMessage());
             }
             e.printStackTrace();
         }
+        return true;
+    }
 
+    public boolean loginUser(String email, String password){
+        DBConnection connection = new DBConnection();
+
+        try {
+            PreparedStatement ps = connection.establishConnection().prepareStatement("SELECT password FROM users WHERE email = ?");
+            ps.setString(1, email);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                if(rs.getString(1).equals(password)){
+                    return true;
+                }
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
